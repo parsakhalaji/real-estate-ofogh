140b108e403b69bef1fc85210328586caf86a549
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-22 21:18:05 +0330

    Create project init routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Templates/Home/Home";
+import Login from "./Components/Templates/Login/Login";
+import Signup from "./Components/Templates/Signup/Signup";
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
