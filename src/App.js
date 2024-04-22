afa63076d4a9bb637b25f452051cbd2e542f762d
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-22 23:13:48 +0330

    Wrap user context provider

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Home from "./Components/Templates/Home/Home";
 import Login from "./Components/Templates/Login/Login";
 import Signup from "./Components/Templates/Signup/Signup";
 import Navbar from "./Components/Modules/Navbar/Navbar";
+import UserContext from "./Contexts/User/UserContext";
+import { useState } from "react";
 
 function App() {
+    const [user, setUser] = useState(null);
+
     return (
         <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
+            <UserContext.Provider value={{ user, setUser }}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </UserContext.Provider>
         </>
     );
 }
