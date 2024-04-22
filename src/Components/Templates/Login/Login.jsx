24ad1ed24908b85f11cffc93a252e73bdecfc6c5
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-22 22:09:21 +0330

    Feat: Develop login and signup templates

--- src/Components/Templates/Login/Login.jsx
+++ src/Components/Templates/Login/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login">
+            <div className="container">
+                <div className="login__wrapper">
+                    <h1 className="login__title">Login</h1>
+                    <p className="login__caption">
+                        Please enter your email and password to login
+                    </p>
+                    <form className="login-form">
+                        <label className="login__label" htmlFor="email">
+                            Email
+                            <input
+                                className="login__input"
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </label>
+                        <label className="login__label" htmlFor="password">
+                            Password
+                            <input
+                                className="login__input"
+                                type="password"
+                                name="password"
+                                id="password"
+                            />
+                        </label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
--- src/Components/Templates/Signup/Signup.jsx
+++ src/Components/Templates/Signup/Signup.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./Signup.css";
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div className="signup">
+            <div className="container">
+                <div className="signup__wrapper">
+                    <h1 className="signup__title">Sign up</h1>
+                    <p className="signup__caption">
+                        Please enter your data to sign up
+                    </p>
+                    <form className="signup-form">
+                        <label className="signup__label" htmlFor="username">
+                            Username
+                            <input
+                                className="signup__input"
+                                type="username"
+                                name="username"
+                                id="username"
+                            />
+                        </label>
+                        <label className="signup__label" htmlFor="email">
+                            Email
+                            <input
+                                className="signup__input"
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </label>
+                        <label className="signup__label" htmlFor="password">
+                            Password
+                            <input
+                                className="signup__input"
+                                type="password"
+                                name="password"
+                                id="password"
+                            />
+                        </label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Signup
+export default Signup;
