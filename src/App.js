e6beb246377abae98c033beec5cd7fb03dbae8bb
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-22 21:38:35 +0330

    create static navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Templates/Home/Home";
 import Login from "./Components/Templates/Login/Login";
 import Signup from "./Components/Templates/Signup/Signup";
+import Navbar from "./Components/Modules/Navbar/Navbar";
 
 function App() {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
--- src/Components/Modules/Navbar/Navbar.jsx
+++ src/Components/Modules/Navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 function Navbar() {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="navbar__wrapper">
+                    <div className="navbar__left">
+                        <ul className="navbar-menu">
+                            <li className="navbar-menu__item">
+                                <Link to="/">Home page</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navbar__right">
+                        <ul className="navbar-menu">
+                            <li className="navbar-menu__item">
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li className="navbar-menu__item">
+                                <Link to="/signup">Signup</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
