97b41a631e2729b4e287c9aa7c46e0c9a5df316f
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-23 17:35:46 +0330

    Wrap MapContainer in a wrapper

--- src/Components/Modules/Location/SelectLoacation.jsx
+++ src/Components/Modules/Location/SelectLoacation.jsx
@@ -2,9 +2,11 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 function SelectLocation() {
     return (
-        <MapContainer center={[35.6892, 51.389]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        </MapContainer>
+        <div className="select-location__wrapper">
+            <MapContainer center={[35.6892, 51.389]} zoom={13}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </MapContainer>
+        </div>
     );
 }
 
