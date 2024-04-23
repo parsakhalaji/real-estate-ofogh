6532241b29d040ff5e77ab36d36dcbe194a59902
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-23 16:47:11 +0330

    Update navbar to dynamically render the items

--- src/Components/Modules/Navbar/Navbar.jsx
+++ src/Components/Modules/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"
+import "./Navbar.css";
+import UserContext from "../../../Contexts/User/UserContext";
 
 function Navbar() {
+    const { user } = useContext(UserContext);
+
     return (
         <div className="navbar">
             <div className="container">
@@ -16,12 +19,27 @@ function Navbar() {
                     </div>
                     <div className="navbar__right">
                         <ul className="navbar-menu">
-                            <li className="navbar-menu__item">
-                                <Link to="/login">Login</Link>
-                            </li>
-                            <li className="navbar-menu__item">
-                                <Link to="/signup">Signup</Link>
-                            </li>
+                            {user ? (
+                                <>
+                                    <li className="navbar-menu__item">
+                                        <Link to="/addnewhouse">
+                                            Add new house
+                                        </Link>
+                                    </li>
+                                    <li className="navbar-menu__item">
+                                        Your account: {user.username}
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="navbar-menu__item">
+                                        <Link to="/login">Login</Link>
+                                    </li>
+                                    <li className="navbar-menu__item">
+                                        <Link to="/signup">Signup</Link>
+                                    </li>
+                                </>
+                            )}
                         </ul>
                     </div>
                 </div>
