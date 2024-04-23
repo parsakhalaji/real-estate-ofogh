026064902b7b13ebcef0ef596e8737ea6200bc1f
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-23 20:05:57 +0330

    Develop Home template component

--- src/Components/Templates/Home/Home.jsx
+++ src/Components/Templates/Home/Home.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const [houses, setHouses] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/houses?_limit=4&_page=${currentPage}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setHouses(data);
+                console.log("data: ", data);
+            })
+            .catch((err) => console.log(err));
+    }, [currentPage]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/houses")
+            .then((res) => res.json())
+            .then((data) => {
+                const totalHouses = data.length;
+                setTotalPages(Math.ceil(totalHouses / 4));
+                console.log("total pages: ", totalPages);
+            });
+    }, [houses]);
+
+    const previousPageHandler = () => {
+        setCurrentPage((prev) => prev - 1);
+    };
+    const nextPageHandler = () => {
+        setCurrentPage((prev) => prev + 1);
+    };
+
+    return (
+        <div className="container">
+            <div className="home">
+                {!houses.length ? (
+                    <h1 className="home__notFound">No house found</h1>
+                ) : (
+                    <div className="home__content">
+                        <ul className="home__houses">
+                            {houses.map((house) => (
+                                <li className="home__house">{house.id}</li>
+                            ))}
+                        </ul>
+                        <div className="home__btns">
+                            <button
+                                onClick={previousPageHandler}
+                                className="btn"
+                            >
+                                Previous
+                            </button>
+                            <button onClick={nextPageHandler} className="btn">
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
