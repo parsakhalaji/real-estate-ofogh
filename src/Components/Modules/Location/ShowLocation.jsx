7ce88332b43eba2f61d46123ea1c5c2b8b04107c
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-24 19:33:37 +0330

    Develop ShowLocation component

--- src/Components/Modules/Location/ShowLocation.jsx
+++ src/Components/Modules/Location/ShowLocation.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "./SelectLocation.css";
 
-function ShowLocation() {
-  return (
-    <div>ShowLocation</div>
-  )
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+});
+
+function ShowLocation({ location }) {
+    return (
+        <MapContainer center={[location.lat, location.lng]} zoom={20}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker
+                position={[location.lat, location.lng]}
+                icon={DefaultIcon}
+            ></Marker>
+        </MapContainer>
+    );
 }
 
-export default ShowLocation
+export default ShowLocation;
