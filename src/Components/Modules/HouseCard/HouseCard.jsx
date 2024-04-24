208aa512bc7044f2c301b2d40ff3816b7dcac2fb
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-24 18:25:43 +0330

    Wrap HouseCard Component with Link

--- src/Components/Modules/HouseCard/HouseCard.jsx
+++ src/Components/Modules/HouseCard/HouseCard.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./HouseCard.css";
+import { Link } from "react-router-dom";
 
 function HouseCard({ data }) {
     return (
-        <div className="house-card">
-            <div className="house-card__img-container">
-                <img
-                    src="./Images/house.jpeg"
-                    alt=""
-                    className="house-card__img"
-                />
+        <Link to={`/houses/${data.id}`}>
+            <div className="house-card">
+                <div className="house-card__img-container">
+                    <img
+                        src="./Images/house.jpeg"
+                        alt=""
+                        className="house-card__img"
+                    />
+                </div>
+                <div className="house-card__content">
+                    <h3 className="house-card__address">{data.address}</h3>
+                </div>
             </div>
-            <div className="house-card__content">
-                <h3 className="house-card__address">{data.address}</h3>
-            </div>
-        </div>
+        </Link>
     );
 }
 
