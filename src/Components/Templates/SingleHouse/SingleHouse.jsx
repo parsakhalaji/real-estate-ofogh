af82e4b440208d05b5a3c8ba60448e13e0f17752
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-24 18:22:14 +0330

    Develop SingleHouse component basically

--- src/Components/Templates/SingleHouse/SingleHouse.jsx
+++ src/Components/Templates/SingleHouse/SingleHouse.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function SingleHouse() {
-  return (
-    <div>SingleHouse</div>
-  )
+    const { houseID } = useParams();
+    const [currenHouse, setCurrentHouse] = useState(null);
+    useEffect(() => {
+        fetch(`http://localhost:4000/houses/${houseID}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setCurrentHouse(data);
+            });
+    }, []);
+    return (
+        <div className="single-house">
+            <h2></h2>
+        </div>
+    );
 }
 
-export default SingleHouse
+export default SingleHouse;
