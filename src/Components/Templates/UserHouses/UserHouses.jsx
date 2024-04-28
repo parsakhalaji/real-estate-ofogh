5988de3bc3ac3a2aaf2b3e2433b2da19f49ad9ba
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-28 20:13:39 +0330

    Create UserHouses component

--- src/Components/Templates/UserHouses/UserHouses.jsx
+++ src/Components/Templates/UserHouses/UserHouses.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "../Home/Home.css";
+import { useParams } from "react-router-dom";
 
+function Home() {
+    const [houses, setHouses] = useState([]);
+    const { userID } = useParams();
 
-function UserHouses() {
-  return (
-    <div>UserHouses</div>
-  )
+    useEffect(() => {
+        fetch("http://localhost:4000/houses")
+            .then((res) => res.json())
+            .then((data) => {
+                const userHouses = data.filter(
+                    (house) => house.userID == userID
+                );
+                setHouses(userHouses);
+                console.log("userhouses: ", userHouses);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="home">
+                {!houses.length ? (
+                    <h1 className="home__notFound">No house found</h1>
+                ) : (
+                    <div className="home__content">
+                        <ul className="home__houses">
+                            {houses.map((house) => (
+                                <li key={house.id} className="home__house">
+                                    <UserHouseCard data={house} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default UserHouses
+export default Home;
