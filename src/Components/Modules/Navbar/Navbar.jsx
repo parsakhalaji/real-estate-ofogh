391016aed62b0067e4553d9d67aaa93a149ff718
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-28 22:41:23 +0330

    Navigate user after submiting the search input

--- src/Components/Modules/Navbar/Navbar.jsx
+++ src/Components/Modules/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import UserContext from "../../../Contexts/User/UserContext";
 
 function Navbar() {
     const { user } = useContext(UserContext);
+    const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        const processedSearchQuery = searchQuery
+            .trim()
+            .toLowerCase()
+            .replace(/\s+/g, "-");
+        navigate(`/search/${processedSearchQuery}`);
+    };
 
     return (
         <div className="navbar">
@@ -16,11 +31,16 @@ function Navbar() {
                                 <Link to="/">Home page</Link>
                             </li>
                             <li className="navbar-menu__item">
-                                <form className="navbar__search">
+                                <form
+                                    className="navbar__search"
+                                    onSubmit={handleSearchSubmit}
+                                >
                                     <input
                                         className="navbar__input"
                                         placeholder="Type to search ..."
                                         type="text"
+                                        value={searchQuery}
+                                        onChange={handleSearchChange}
                                     />
                                 </form>
                             </li>
